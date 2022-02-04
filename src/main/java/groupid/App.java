ef41ec5e37138a32323dba6a9f06698542c750cb
vvinaybhargav <78915448+vvinaybhargav@users.noreply.github.com>
2022-02-04 12:26:52 -0500

    new

---
+++ src/main/java/groupid/App.java
@@ -0,0 +1,13 @@
+package groupid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
